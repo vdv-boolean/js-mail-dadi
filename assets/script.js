ea209b88ff3c5e62fb67759a23548551f000cd72
vincenzodivita <divitavincenzo@icloud.com>
2023-04-17 15:20:57 +0200

    Add if for check if user mail is in the array

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,19 @@
 // Request mail address
 const requestMail = prompt("Inserire indirizzo mail");
 //Array of accepted mail addres
-const mailArray = ["vincenzo@gmail.com", "gianluca@live.it", "irene@libero.it", "andrea@gmail.com"]
+const mailArray = ['vincenzo@gmail.com', 'gianluca@live.it', 'irene@libero.it', 'andrea@gmail.com']
 
+// Check user input and mail addres
 console.log("User mail: " + requestMail)
 console.log("Mail array: " + mailArray)
+
+//Check if user mail is in the array
+if (mailArray.includes(requestMail)) {
+    console.log('%cMail accepted', 'color: green')
+} else {
+    console.log('%cMail not accepted', 'color: red');  
+}
+
+
+
+
