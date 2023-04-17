6b46ffa00c9b8bb376e5d51eb9a939015cf2b603
vincenzodivita <divitavincenzo@icloud.com>
2023-04-17 14:49:45 +0200

    Add accepted mail address array

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,7 @@
 // Request mail address
 const requestMail = prompt("Inserire indirizzo mail");
+//Array of accepted mail addres
+const mailArray = ["vincenzo@gmail.com", "gianluca@live.it", "irene@libero.it", "andrea@gmail.com"]
 
-console.log(requestMail)
+console.log("User mail: " + requestMail)
+console.log("Mail array: " + mailArray)
