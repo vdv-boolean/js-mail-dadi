ed0034cfa180454215b1d3d4368a2fc9503c4a7f
vincenzodivita <divitavincenzo@icloud.com>
2023-04-17 23:47:24 +0200

    Change prompt with querySelector

--- mail_checker/assets/script.js
+++ mail_checker/assets/script.js
@@ -1,17 +1,21 @@
 // Request mail address
-const requestMail = prompt("Inserire indirizzo mail");
+//const requestMail = prompt("Inserire indirizzo mail");
+const requestMail = document.querySelector('input');
+
 //Array of accepted mail addres
 const mailArray = ['vincenzo@gmail.com', 'gianluca@live.it', 'irene@libero.it', 'andrea@gmail.com'];
 
+const btn = document.querySelector("button"); 
+btn.addEventListener('click', function () {
 // Check user input and mail addres
-console.log("User mail: " + requestMail)
+console.log("User mail: " + requestMail.value)
 console.log("Mail array: " + mailArray)
 
 //Check if user mail is in the array
 let check = false;
 
 for (let i = 0; i < mailArray.length; i++){
-if (requestMail == mailArray[i]) {
+if (requestMail.value == mailArray[i]) {
     check = true;
     console.log('%cMail accepted', 'color: green');
     const welcomeMsg = alert("Welcome");
@@ -22,3 +26,4 @@ if (check == false){
     console.log('%cMail not accepted', 'color: red');  
     const accessDeniedMsg = alert("Acces denied!");
 }
+})
